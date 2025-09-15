c1081dfaa9c6c67f3b356edd715beec721a77156
dgpluks <danielgomespereira2006@gmail.com>
2025-09-15 15:11:50 -0300

    Add files via upload

--- atividade2.4.py
+++ atividade2.4.py
@@ -2,33 +2,16 @@ val = int(input())
 pag = int(input())
 troc = val - pag
 troco = [0] * 6 
+nota = [50,20,10,5,2,1]
 if(pag>val):
     print("valor pago é insuficiente\n")
 else:
-    while(troc > 50):
-        troc = troc-50
-        troco[0]+=1
-    while(troc > 20):
-        troc = troc-20
-        troco[1]+=1
-    while(troc > 10):
-        troc = troc-10
-        troco[2]+=1
-    while(troc > 5):
-        troc = troc-5
-        troco[3]+=1
-    while(troc > 2):
-        troc = troc-2
-        troco[4]+=1
-    while(troc >= 1):
-        troc = troc-1
-        troco[5]+=1
+    for i in range(6):
+        while troc >= nota[i]:
+            troc = troc-nota[i]
+            troco[i]+=1
     troc = 0
-    troc += troco[0]*50
-    troc += troco[1]*20
-    troc += troco[2]*10
-    troc += troco[3]*5
-    troc += troco[4]*2
-    troc += troco[5]*1
-    print(troc)
-
+    for i in range(6):
+        troc += troco[i]*nota[i]
+        print("serão pagas ", troco[i], "notas de ", nota[i])
+    print("Valor a ser pago é: ",  troc)
