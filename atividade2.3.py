099895466d7ec040c7c028c4af5eedba88942386
dgpluks <danielgomespereira2006@gmail.com>
2025-09-15 14:16:18 -0300

    Add files via upload

--- atividade2.3.py
+++ atividade2.3.py
@@ -1,18 +1,18 @@
-a = int(input())
-b = int(input())
-c = int(input())
+a = float(input())
+b = float(input())
+c = float(input())
 if a == 0 and b == 0 and c == 0:
     print("Igualdade confirmada: 0 = 0\n")
 elif a == 0 and b == 0:
     print("Coeficientes informados incorretamente\n")
 elif a == 0:
     print("Esta é uma equação de primeiro grau\n")
-elif 
+else: 
     print("Esta é uma equação de segundo grau\n")
     disc = b**2- 4*a*c
     if disc == 0:
-        print("Esta equação possui duas raízes reais iguais\n")
+        print("Esta é uma equação de segundo grau. Esta equação possui duas raízes reais iguais: x' = x'' = ",(b*-1)/(2*a))
     elif disc > 0:
-        print("Esta equação possui duas raízes reais diferentes\n")
-    else
-        print("Esta equação não possui raízes reais\n")
+        print("Esta é uma equação de segundo grau. Esta equação possui duas raízes reais diferentes: delta = ",disc ,", x' =" ,    round((-b+disc**(1/2))/(2*a),2),", x'' = ",round((-b-disc**(1/2))/(2*a),2))
+    else:
+        print("Esta é uma equação de segundo grau. Esta equação não possui raízes reais (delta < 0): delta = ", round(disc))
